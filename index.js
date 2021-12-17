d7b430ed5e34b5f77f58710f86a2e8848daa2f43
hiltonjrlucas <hiltonjr.lucas@gmail.com>
2021-12-17 00:50:08 -0300

    1 file modified

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const str = 'ÁÉÍÓÚáéíóúâêîôûàèìòùÇç/.,~!@#$%&_-12345';
 
 console.log(str.normalize('NFD').replace(/[\u0300-\u036f]/g, ''));
 console.log(str.normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z\s])/g, ''));
-console.log('Esta é uma frase!'.replace(/[^\w\-]+/g, '-'))
+console.log('Esta é-uma frase!'.replace(/[^\w\-]+/g, '*'))
 console.log('Esta é uma frase!'.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[^\w\-]+/g, '-'))
 
 // String Á em UTF-18 tem 1 dígito
